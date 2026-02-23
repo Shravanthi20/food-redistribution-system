71b6f9d24cfdfe9aecbe986855ee123d7a467ef2
SISIR-REDDY <sisirreddy771@gmail.com>
2026-02-23 10:29:00 +0530

    auth: Add NGO registration screen
     NGO Registration
     Non-profit organization registration form
     Registration number and legal documentation
     Mission statement and service area details
     Beneficiary count and operational information

---
+++ food_redistribution_app/lib/screens/auth/ngo_registration_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NGORegistrationScreen extends StatelessWidget {
+  const NGORegistrationScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('NGO Registration'),
+      ),
+      body: const Center(
+        child: Text('NGO Registration Screen - To be implemented'),
+      ),
+    );
+  }
+}
