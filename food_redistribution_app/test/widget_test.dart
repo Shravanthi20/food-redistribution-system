6f67bd7815ffb78ea4f1e0f02cf080966975152e
Akshitha1208 <lakshmiakshithad@gmail.com>
2026-03-06 19:12:28 +0530

    Make widget_test independent of app Firebase initialization

--- food_redistribution_app/test/widget_test.dart
+++ food_redistribution_app/test/widget_test.dart
@@ -1,21 +1,10 @@
-import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:food_redistribution_app/main.dart';
-
 void main() {
-  setUpAll(() async {
-    const MethodChannel pathChannel =
-        MethodChannel('plugins.flutter.io/path_provider');
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-        .setMockMethodCallHandler(pathChannel, (MethodCall methodCall) async {
-      return '.';
-    });
-  });
-
-  testWidgets('App smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const FoodRedistributionApp());
+  testWidgets('Widget smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: SizedBox()));
     await tester.pumpAndSettle();
-    expect(find.byType(FoodRedistributionApp), findsOneWidget);
+    expect(find.byType(MaterialApp), findsOneWidget);
   });
 }
