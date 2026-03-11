9081a314e587ca0b02ba6d888bfdab2727410953
Shravanthi20 <satyanar061@gmail.com>
2026-03-11 11:14:08 +0530

    Rectified Maps API issue

--- food_redistribution_app/ios/Runner/AppDelegate.swift
+++ food_redistribution_app/ios/Runner/AppDelegate.swift
@@ -1,4 +1,5 @@
 import Flutter
+import GoogleMaps
 import UIKit
 
 @main
@@ -7,6 +8,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyAdTz-dn_-vNofCjkJTWZ90vOg1PcioQ5Y")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
